a223eb999254b90568219fd1e108b74b0e437666
ashish-panicker <ashish.s.panicker@proton.me>
2025-11-12 18:54:58 +0530

    Order to inventory using RestTemplate

--- order-service/src/main/java/com/example/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/example/orderservice/service/OrderService.java
@@ -3,6 +3,7 @@ package com.example.orderservice.service;
 import com.example.orderservice.model.Order;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class OrderService {
@@ -10,8 +11,22 @@ public class OrderService {
     @Value("${inventory.service.url}")
     private String inventoryUrl;
 
+    private final RestTemplate restTemplate;
+
+    public OrderService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
     public Order placeOrder(Order order) {
-        return null;
+        Boolean available = restTemplate.getForObject(
+                inventoryUrl + "/check?code=" + order.productCode() + "&qty=" + order.quantity(),
+                Boolean.class);
+
+        if (Boolean.TRUE.equals(available)) {
+            return new Order(order.id(), order.productCode(), order.quantity(), "ORDER_PLACED");
+        } else {
+            return new Order(order.id(), order.productCode(), order.quantity(), "OUT_OF_STOCK");
+        }
     }
 
 }
